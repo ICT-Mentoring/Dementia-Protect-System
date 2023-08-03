ab3e729ffc4fa9f426caa28eb14bc7ef3922a691
sejeonghwang <tpwjddl987@naver.com>
2023-08-04 00:20:18 +0900

    로그인폼 UI

--- frontend/src/pages/PageLogin.js
+++ frontend/src/pages/PageLogin.js
@@ -1,23 +1,26 @@
 import React from "react";
 import { Helmet } from "react-helmet-async";
+import styles from "../style/PageLogin.css";
 
 function PageLogin() {
   return (
-    <div>
+    <div className="login">
       <Helmet>
         <title>로그인 | 치매노인 보호 시스템</title>
       </Helmet>
       <h1>로그인 페이지</h1>
-      <div>
-        <label>아이디</label>
-        <br />
-        <input />
-        <br />
-        <br />
-        <label>비밀번호</label>
-        <br />
-        <input />
-      </div>
+        <div className="form">
+          <label>아이디</label>
+          <br /><br />
+          <input type="text" className={styles.user_id} required placeholder="ID" />
+          <br />
+          <br />
+          <label>비밀번호</label>
+          <br /><br />
+          <input type="password" className={styles.user_pw} required placeholder="PASSWORD" />
+          <br /><br />
+          <button>Login</button>
+        </div>
     </div>
   );
 }
